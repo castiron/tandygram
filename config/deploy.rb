68e793c15dd02519c1d1c9633a8f722fa942dffe
Gabe Blair <gabe@castironcoding.com>
2016-04-15 17:54:43 -0700

    [F] Add Capistrano task to reset data
    Backs it up first ;)

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,13 +7,24 @@ set :deploy_to, -> { "/home/#{fetch(:application)}/deploy/" }
 set :log_level, :info
 
 namespace :deploy do
-
   after :restart, :clear_cache do
     on roles(:app), in: :groups, limit: 3, wait: 10 do
-	execute :sudo, :service, :tandygram, :restart
+      execute :sudo, :service, :tandygram, :restart
     end
   end
+end
 
+namespace :tandygram do
+  task :reset do
+    on roles(:app), in: :groups, limit: 3, wait: 10 do
+      bak_dir = "/home/#{fetch(:application)}/tiedot-bak"
+
+      execute :mkdir, '-p', bak_dir
+      execute :sudo, :service, :tandygram, :stop
+      execute :mv, '/tmp/tandy-tiedot', "#{bak_dir}/bak-#{Time.now.to_i}"
+      execute :sudo, :service, :tandygram, :start
+    end
+  end
 end
 
 after 'deploy:publishing', 'deploy:restart'
